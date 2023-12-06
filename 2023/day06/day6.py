df5a5aca87b886086ddc09b68eb05bc1e54bfbe7
Uwe Schmidt <derFeind87@gmail.com>
2023-12-06 05:48:30 +0100

    add day 6

---
+++ 2023/day06/day6.py
@@ -0,0 +1,14 @@
+
+import re 
+
+file = open('day5.in', 'r')
+file = open('day5-ex.in', 'r')
+lines = file.readlines()
+ans1 = 0
+ans2 = 0
+
+for i,l in enumerate(lines):
+    print(i,l)
+print("----")
+print("ans1:",ans1)
+print("ans2:",ans2)
