197be1cc8cb9a87feb5b36418beb3f1f8165229c
Uwe Schmidt <derFeind87@gmail.com>
2023-12-12 06:59:19 +0100

    day12

---
+++ 2015/day02/day2.cpp
@@ -0,0 +1,21 @@
+// author: Uwe Schmidt
+// created: 2023-09-30 10:51:49.188933
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define forn(i, n) for (int i = 0; i < int (n); i++)
+#define ll long long
+ 
+const char *alphabet = "abcdefghijklmnopqrstuvwxyz";
+ 
+void
+run_case ()
+{
+}
+ 
+ 
+int
+main ()
+{
+ 
+}
--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -3,13 +3,52 @@ import math
 import sys
 import numpy as np
 import copy 
+from itertools import combinations 
 ans1 = 0
 ans2 = 0
 
 F = open(sys.argv[1], "r")
 lines = [l.strip() for l in F.readlines()]
+springs = []
+order = []
+SO = []
+
+def valid(inp,o):
+    if inp.count("#") != sum(o):
+        return False 
+    
+
+def solve(inp):
+    s,o = inp 
+    segs = []
+    temp = ""
+    r = []
+    for c in s:
+        if c != ".":
+            temp += c 
+        else: 
+            if temp != "":
+                segs.append(temp)
+                temp = ""
+    if temp != "":
+        segs.append(temp)
+    for c in combinations("ABCD",2):
+        print(c)
+
+    print(segs,r)
+
+        
+
 for l in lines:
     print(l)
+    S,O = l.split()
+    springs.append(S)
+    order.append(O.split())
+    (solve((S,O)))
+    SO.append((springs[-1],order[-1]))
+
+for s in SO:
+    print(s)
 
 print("------")
 print("ans1:", ans1)
