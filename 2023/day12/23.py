3bda4fa9d11a318d26dee20e3440bf660a3cedce
uewekenuewe <derFeind87@gmail.com>
2024-11-27 14:36:42 +0100

    cobol template + 2023 part a in cobol

---
+++ 2023/day12/23.py
@@ -0,0 +1,16 @@
+f = open("12","r")
+lines = f.readlines()
+
+def processLine(arg1, arg2):
+    if len(arg1) == 0 or len(arg2) == 0:
+        return 0
+
+
+
+
+print(processLine("#.",[1]))
+
+
+#for l in lines:
+#    l_spl = l.split()
+#    print(processLine(l_spl[0],[int(x) for x in l_spl[1].split(",")]))
