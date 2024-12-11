dd8d14db4ae32a531000349f46cb52ded3d92ded
uewekenuewe <derFeind87@gmail.com>
2024-12-11 11:26:39 +0100

    Update day11.py
    part2

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -13,19 +13,19 @@ else:
     f = open("11", "r")
 
 
-cache = {}
+cachee = {}
 
-def stone_cache(num):
-    if num in cache.keys():
-        return cache[num]
+def stone_cachee(num):
+    if num in cachee.keys():
+        return cachee[num]
     if num == 0:
-        cache[num] = [1]
+        cachee[num] = [1]
         return [1]
     if len(str(num)) % 2 == 0:
         num = str(num)
-        cache[num] = [int(num[:int(len(str(num))/2)]), int(num[int(len(str(num))/2):])]
+        cachee[num] = [int(num[:int(len(str(num))/2)]), int(num[int(len(str(num))/2):])]
         return [int(num[:int(len(str(num))/2)]), int(num[int(len(str(num))/2):])]
-    cache[num] = [num*2024]
+    cachee[num] = [num*2024]
     return [num*2024]
 
 def stone(num):
@@ -34,16 +34,6 @@ def stone(num):
     if len(str(num)) % 2 == 0:
         num = str(num)
         return [int(num[:int(len(str(num))/2)]), int(num[int(len(str(num))/2):])]
-    return [num*2024]
-
-def stone2(num):
-    if num == 0:
-        return 1
-    if len(str(num)) % 2 == 0 and len(str(num)) > 1:
-        num = str(num)
-        return [int(num[:int(len(str(num))/2)]), int(num[int(len(str(num))/2):])]
-    if len(str(num)) % 2 == 0 and len(str(num)) == 1:
-        return 1
     return [num*2024]
 
 lines = [l.strip() for l in f.readlines()]
@@ -60,20 +50,40 @@ for _ in range(25):
     s = copy.deepcopy(scpy)
 
 
+def calcCache():
+    r = 0
+    for x in cache:
+        if cache[x][0] > 0:
+            r += cache[x][0]
+    return r
+
+
 ans1 = len(s)
 
 levels = [0 for _ in range(75)]
-s = [2024]
+s = sorg
+cache = {}
+for x in s:
+    cache[x] = [1,stone(x)]
+#s = [[1,x] for x in s]
 for _ in range(75):
-    scpy = []
-    for x in range(len(s)):
-        scpy += stone(s[x])
-    s = copy.deepcopy(scpy)
-    #print(s)
+    cache_cpy = {}
+    for c in cache:
+        v = stone(c)
+        if cache[c][0] > 0:
+            for x in cache[c][1]:
+                if x in cache_cpy.keys():
+                    cache_cpy[x][0] += cache[c][0]
+                else:
+                    cache_cpy[x] = [cache[c][0],stone(x)]
+
+    cache = cache_cpy
 
 
 
-ans2 = len(s)
+for c in cache:
+    if cache[c][0] > 0:
+        ans2 += cache[c][0]
 
 
 
